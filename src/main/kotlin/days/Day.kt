1295b60b822b880961b8331728abe4aab671e755
Thomas Poulain <thomas.poulain.partner@decathlon.com>
2023-12-05 15:11:13 +0100

    chore: cleaning

--- src/main/kotlin/days/Day.kt
+++ src/main/kotlin/days/Day.kt
@@ -1,19 +1,18 @@
 package days
 
+import java.io.File
+
 abstract class Day(private val day: Int, private val name: String) {
 
-    private val input: List<String> = Day::class.java.getResource("/day$day.txt")!!.readText().split("\n")
+    private val input: List<String> = File("src/main/resources/day$day.txt").readLines()
 
     abstract fun solvePart1(input: List<String>): String
     abstract fun solvePart2(input: List<String>): String
 
     fun solve() {
         println("\nDay $day - $name")
-        val resultPartOne = solvePart1(input)
-        println("Part One: $resultPartOne")
-
-        val resultParTwo = solvePart2(input)
-        println("Part Two: $resultParTwo")
+        println("Part One: ${solvePart1(input)}")
+        println("Part Two: ${solvePart2(input)}")
     }
 
 }
