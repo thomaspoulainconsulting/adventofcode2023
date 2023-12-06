d84c327255e5296953618186623e4cd784dd5e5e
Thomas Poulain <thomas.poulain.partner@decathlon.com>
2023-12-06 23:42:27 +0100

    chore: init day 4 - part 1

---
+++ src/main/kotlin/days/Day4.kt
@@ -0,0 +1,11 @@
+package days
+
+class Day4: Day(4, "Scratchcards") {
+    override fun solvePart1(input: List<String>): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun solvePart2(input: List<String>): String {
+        TODO("Not yet implemented")
+    }
+}
